f7c88cd18ef5a82c485e916383c5496ce7244596
Gaurav <gauravvala492@gmail.com>
2024-05-05 21:23:32 +0530

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
 import tailwind from "@astrojs/tailwind";
 
 import netlify from "@astrojs/netlify";
@@ -9,6 +9,6 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic(), tailwind()],
-  output: 'hybrid',
-  adapter: netlify()
+  output: "server",
+  adapter: netlify(),
 });
